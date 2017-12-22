c62f6b3882d39dd5bac43f61127172a670b2c1df
brickatyourfeet <brickatyourfeet@gmail.com>
2017-12-21 21:27:52 -0800

    add header component and auth0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Keyboard from './components/Keyboard.js'
+import Auth0Lock from 'auth0-lock'
+import {Button} from 'react-materialize'
+import Header from './components/Header.js'
 
 class App extends Component {
   constructor(props){
@@ -20,9 +23,43 @@ class App extends Component {
     this.setState(situation)
   }
 
+  static defaultProps = {
+    clientId: 'oZl3hH2eA3ohIHz62cQqNgCh9DQUzrSq',
+    domain: 'emojiapi.auth0.com'
+  }
+
+  componentWillMount(){
+    this.lock = new Auth0Lock(this.props.clientId, this.props.domain)
+
+    this.lock.on('authenticated', (authResult)=>{
+      console.log(authResult)
+      this.lock.getUserInfo(authResult.accessToken, (error, profile)=>{
+        
+        if(error) {
+          console.log(error)
+          return
+        }
+        //send to api server
+        //if email log user else post signup
+        //log in -  return id from db
+        //store id in local storage
+        console.log(profile)
+
+        this.setData(authResult.accessToken, profile)
+        
+      })
+    
+    })
+  }
+
+  showModal(){
+    this.lock.show()
+  }
+
   render() {
     return (
       <div className="App">
+      <Header loginClick={this.showModal.bind(this)}/>
         <header className="App-header">
           <h1 className="App-title">Welcome to Emoji App</h1>
         </header>
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {Button, Navbar, Nav, NavItem} from 'react-materialize'
+
+class Header extends Component {
+    loginClick(){
+        //console.log('clickin')
+        this.props.loginClick()
+    }
+    
+    render(){
+        return (
+       
+            <NavItem onClick={this.loginClick.bind(this)} href="#">Login</NavItem>
+   
+        )
+    }
+
+}
+
+export default Header
