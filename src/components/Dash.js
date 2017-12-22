20a6b57703e639008dace4c826e3203e7f937df5
brickatyourfeet <brickatyourfeet@gmail.com>
2017-12-22 14:22:58 -0800

    add user info to dashboard page

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Spell from './Spell'
+import {Card, CardTitle} from 'react-materialize'
 
 class Dash extends Component {
   render() {
@@ -9,6 +10,11 @@ class Dash extends Component {
     <h2>and link to below</h2>
     <p>dashboard will contain link to 'quiz' page and potentially stories?</p>
     <p>could have separate profile page, or just include some info on this dashboard page? </p>
+    <Card className='small'
+    header={<CardTitle image={this.props.profile.picture}>Profile</CardTitle>}
+    actions={[<a href='#'>This is a Link</a>]}>
+    <h3>{this.props.profile.nickname}</h3>
+  </Card>
     <Spell />
     </div>
     
