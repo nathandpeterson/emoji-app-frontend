5b8ce4981a973f3774bfa38ecd6ab3a225053880
Nathan Daniel Peterson <natperson@gmail.com>
2017-12-28 19:54:22 -0800

    start basic landing page, design

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react'
 class Landing extends Component {
   render() {
     return (
-    <h1>attractive landing page</h1>
-    );
+    <div className='landing'>
+    <h1 className='emoji-med'>🐱</h1>
+    <h1>Hello</h1>
+    </div>
+    )
   }
 }
 
